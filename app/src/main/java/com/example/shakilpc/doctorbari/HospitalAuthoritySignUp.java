92b93f289898941262e86377fbf25f4fd8140999
Zahid Hossain <zahidhossain0033@gmail.com>
2017-11-26 22:09:36 +0600

    All signup pages added

---
+++ app/src/main/java/com/example/shakilpc/doctorbari/HospitalAuthoritySignUp.java
@@ -0,0 +1,13 @@
+package com.example.shakilpc.doctorbari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HospitalAuthoritySignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_authority_sign_up);
+    }
+}
